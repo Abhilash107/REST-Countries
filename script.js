9bd2691ef5bcf00364bac527dbcbce42028364ec
Abhilash107 <abhilash00003108884@gmail.com>
2024-03-30 09:54:28 +0530

    added animations

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
-// fetch('https://restcountries.com/v3.1/all')
-// .then( (res) =>res.json())
-// .then(   (data) =>{
-//     data.forEach( (country) =>{
-//         console.log(country);
+const countryContainer= document.querySelector(".countries-container");
 
 
-//     } )
-// } )
+
+fetch('https://restcountries.com/v3.1/all')
+.then( (res) =>res.json())
+.then(   (data) =>{
+    data.forEach( (country) =>{
+        console.log(country.name.common);
+        const countryCard = document.createElement('a');
+        countryCard.classList.add('country-card');
 
 
-const countryCard = document.createElement('a');
-countryCard.classList.add('countryCard');
-// const cardImg = document.createElement('img');
-// cardImg.src = 'https://flagcdn.com/er.svg';
-// countryCard.append(cardImg);
-
-const cardHTML = `
-<img src="https://flagcdn.com/er.svg" alt="flag">
-                <div class="card-text">
-                    <h3>Cyprus</h3>
-                    <p><b>Population: </b>12323554</p>
-                    <p><b>Region:  </b>Europe</p>
-                    <p><b>Capital: </b>Berlin</p>
-                </div>
-                
-            </a>`;
+    const cardHTML = `
+    <img src="${country.flags.svg}" alt="flag">
+            <div class="card-text">
+                <h3>${country.name.common}</h3>
+                <p><b>Population: </b>${country.population.toLocaleString('en-In')}</p>
+                <p><b>Region:  </b>${country.region}</p>
+                <p><b>Capital: </b>${country.capital}</p>
+            </div>`;
 
 countryCard.innerHTML = cardHTML;
+countryContainer.append(countryCard);
+
+       
+
+    } )
+} )
+
+// const countryCard = document.createElement('a');
+// countryCard.classList.add('country-card');
+
+
+// const cardHTML = `
+// <img src="https://flagcdn.com/er.svg" alt="flag">
+//                 <div class="card-text">
+//                     <h3>Cyprus</h3>
+//                     <p><b>Population: </b>12323554</p>
+//                     <p><b>Region:  </b>Europe</p>
+//                     <p><b>Capital: </b>Berlin</p>
+//                 </div>
+                
+//             </a>`;
+
+// countryCard.innerHTML = cardHTML;
+// countryContainer.append(countryCard);
+
+
+
+
 
 
 
