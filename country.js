92cd0fa235e17c62abfd8aac014d84a62c0f3c88
Abhilash107 <abhilash00003108884@gmail.com>
2024-04-01 09:37:02 +0530

    Added some files

--- country.js
+++ country.js
@@ -1,6 +1,12 @@
 const countryName = new URLSearchParams(window.location.search).get('name');
+const flagIMG = document.querySelector('.country-details img')
+const countryNameH1= document.querySelector('.country-details h1')
+
+
   fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
   .then(  (res) => res.json() )
-  .then((data) =>{
-    console.log(data);
+  .then(([country]) =>{
+    
+    flagIMG.src= country.flags.svg
+    countryNameH1.innerText=country.name.common
   });
